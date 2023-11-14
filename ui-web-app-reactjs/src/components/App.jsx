eb358f59066c09fc431967cbf309d94eb731848d
Pa1-12 <116073752+Pa1-12@users.noreply.github.com>
2023-11-14 11:22:50 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [displayData, setDisplayData] = useState({url:''})
 
   const handleApiCall = async (e) => {
-    const url = 'http://localhost:9999/'+e.target.name;
+    const url = 'http://18.233.226.216/'+e.target.name;
     displayData.url=url
     let options = {}
       try {
